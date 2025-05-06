07ca4bc3692081dcebeed8f5733a4e46d6e6be71
carter-oleary <carter.m.oleary@outlook.com>
2025-05-06 13:50:06 -0400

    Assignment 5.2

---
+++ Assignment 5.2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Assignment 5.2.1
+Console.WriteLine("----Assignment 5.2.1----");
+string[] arr = { "Hello World", "fly me to the moon" };
+foreach (string str in arr)
+{
+    Console.WriteLine($"The length of the last word in {str} is {AssignmentFunctions.LastWordLength(str)}");
+}
+Console.WriteLine("\n");
+
+
+// Assignment 5.2.2
+Console.WriteLine("----Assignment 5.2.2----");
+Console.Write("Enter the number of numbers to print: ");
+int n = Convert.ToInt32(Console.ReadLine());
+AssignmentFunctions.RecursiveHeadPrint(n);
+Console.WriteLine("\n\n");
+
+
+// Assignment 5.2.3
+Console.WriteLine("----Assignment 5.2.3----");
+Console.Write("Enter the number of numbers to print: ");
+n = Convert.ToInt32(Console.ReadLine());
+AssignmentFunctions.RecursiveTailPrint(n);
+Console.WriteLine("\n\n");
+
+
+// Assignment 5.2.4
+Console.WriteLine("----Assignment 5.2.4----");
+string[] s = { "RADAR", "Banana", "Anna" };
+foreach(string str in s)
+{
+    if (AssignmentFunctions.RecursivePalindrome(str)) Console.WriteLine($"{str} is a palidrome.");
+    else Console.WriteLine($"{str} is not a palindrome.");
+}
+
+
