88f910cfbaef2602073c9c45b8a15589e8d74f16
carter-oleary <carter.m.oleary@outlook.com>
2025-05-07 12:58:33 -0400

    Added output screenshot

--- Assignment_5.3/Program.cs
+++ Assignment_5.3/Program.cs
@@ -5,4 +5,9 @@ Console.WriteLine(AssignmentFunctions.Flowerbed(arr, 1));
 Console.WriteLine(AssignmentFunctions.Flowerbed(arr, 2));
 
 // Assingnment 5.3.2
-Console.WriteLine("----Assignment 5.3.2----");
+Console.WriteLine("\n----Assignment 5.3.2----");
+arr = [ 2,  3, 5, 7, 10];
+foreach (int i in arr)
+{
+    Console.WriteLine($"There  are {AssignmentFunctions.ClimbStairs(i)} ways to climb {i} stairs.");
+}
