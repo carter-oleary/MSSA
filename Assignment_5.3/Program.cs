0c7e521b627f23a2d5799a393197e14eae82f01f
carter-oleary <carter.m.oleary@outlook.com>
2025-05-07 12:55:29 -0400

    Created Assignment 5.3

---
+++ Assignment_5.3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Assingnment 5.3.1
+Console.WriteLine("----Assignment 5.3.1----");
+int[] arr = { 1, 0, 0, 0, 1 };
+Console.WriteLine(AssignmentFunctions.Flowerbed(arr, 1));
+Console.WriteLine(AssignmentFunctions.Flowerbed(arr, 2));
+
+// Assingnment 5.3.2
+Console.WriteLine("----Assignment 5.3.2----");
