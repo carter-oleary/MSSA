3964627d03de1cef9451e300f19f20f72ef75400
carter-oleary <carter.m.oleary@outlook.com>
2025-05-14 11:11:39 -0400

    Assignment 6.3

---
+++ Assignment_6.3/MyQueue.cs
@@ -0,0 +1,30 @@
+﻿public class MyQueue<T>
+{
+    public LinkedList<T> Queue { get; set; }
+
+    public MyQueue()
+    {
+        Queue = new LinkedList<T>();
+    }
+
+    public void Enqueue(T item)
+    {
+        Queue.AddLast(item);
+    }
+
+    public T Dequeue()
+    {
+        T item = Queue.First.Value;
+        Queue.RemoveFirst();
+        return item;
+    }
+
+    public void Display()
+    {
+        foreach (var item in Queue)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Assignment_6.3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Assignment 6.3
+Console.WriteLine("----Assignment 6.3----");
+MyQueue<int> myQueue = new MyQueue<int>();
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Enqueuing {i+1}...");
+    myQueue.Enqueue(i+1);
+}
+myQueue.Display();
+Console.WriteLine($"Dequeing... " + myQueue.Dequeue());
+Console.WriteLine($"Dequeing... " + myQueue.Dequeue());
+myQueue.Display();
