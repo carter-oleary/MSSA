185e75019f8563823f82d80aa7d3b6b112a88a0a
carter-oleary <carter.m.oleary@outlook.com>
2025-05-19 12:22:04 -0400

    Assignment 7.1

---
+++ Assignment_7.1/Functions.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+public static class Functions
+{
+    public static void SelectionSort(int[] arr)
+    {
+        int minPos = 0;
+        int temp = 0;
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            minPos = i;
+            for (int j = i + 1; j < arr.Length; j++) { 
+                if( arr[j] < arr[minPos])
+                {
+                    minPos = j;
+                }
+                
+            }
+            temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static string MergeStrings(string s1, string s2)
+    {
+        int minLen = Math.Min(s1.Length, s2.Length);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < minLen; i++)
+        {
+            sb.Append(s1[i]);
+            sb.Append(s2[i]);
+        }
+        if (s1.Length == s2.Length) return sb.ToString();
+        else if (s1.Length < s2.Length) sb.Append(s2.Substring(minLen));
+        else sb.Append(s1.Substring(minLen));
+        return sb.ToString();
+    }
+
+    public static string PrintArray(int[] arr)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("[ ");
+        foreach (int i in arr) { sb.Append(i + " "); }
+        sb.Append("]");
+        return sb.ToString();
+    }
+}
---
+++ Assignment_7.1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Assignment 7.1.1
+Console.WriteLine("----Assignment 7.1.1----");
+Random r = new Random();
+int n = 20;
+int[] grades = new int[n];
+for (int i = 0; i < n; i++) grades[i] = r.Next(40, 100);
+Console.WriteLine($"Initial array: {Functions.PrintArray(grades)}");
+Functions.SelectionSort(grades);
+Console.WriteLine($"Sorted array: {Functions.PrintArray(grades)}\n");
+
+
+// Assignment 7.1.2
+Console.WriteLine("----Assignment 7.1.2----");
+string s1 = "abc";
+string s2 = "pqr";
+string s3 = "ab";
+string s4 = "pqrs";
+string com1 = Functions.MergeStrings(s1, s2);
+string com2 = Functions.MergeStrings(s3, s4);
+Console.WriteLine($"Word 1: {s1}\nWord 2: {s2}\nCombined: {com1}");
+Console.WriteLine($"Word 1: {s3}\nWord 2: {s4}\nCombined: {com2}");
+
+
+Console.ReadKey();
