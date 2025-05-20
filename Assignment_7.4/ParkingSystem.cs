7073a7df68fa69a078871e46ee49325f2f48c8ba
carter-oleary <carter.m.oleary@outlook.com>
2025-05-20 17:13:00 -0400

    Assignment 7.4

---
+++ Assignment_7.4/ParkingSystem.cs
@@ -0,0 +1,43 @@
+﻿public class ParkingSystem
+{
+    private int big;
+    private int medium;
+    private int small;
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public bool AddCar(int carType)
+    {
+        switch (carType)
+        {
+            case 1:
+                if(big > 0)
+                {
+                    big--;
+                    return true;
+                }
+                return false;
+            case 2:
+                if (medium > 0)
+                {
+                    big--;
+                    return true;
+                }
+                return false;
+            case 3:
+                if (big > 0)
+                {
+                    big--;
+                    return true;
+                }
+                return false;
+            default:
+                return false;
+        }
+    }
+}
---
+++ Assignment_7.4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Assignment 7.4
+Console.WriteLine("----Assignment 7.4----");
+ParkingSystem ps = new ParkingSystem(1, 1, 0);
+int[] cars = [1, 2, 3, 1];
+foreach (int car in cars)
+{
+    string result = (ps.AddCar(car)) ? "Car successfully parked" : "That car won't fit";
+    Console.WriteLine(result);
+}
