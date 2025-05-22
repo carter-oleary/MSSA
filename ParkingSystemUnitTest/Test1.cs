cb9b816a53d97ceb2b17c9507f9ea0edc6e2521d
carter-oleary <carter.m.oleary@outlook.com>
2025-05-22 15:29:24 -0400

    Added unit testing

---
+++ ParkingSystemUnitTest/Test1.cs
@@ -0,0 +1,25 @@
+﻿namespace ParkingSystemUnitTest
+{
+    [TestClass]
+    public sealed class CarCanBeAdded
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ParkingSystem ps = new ParkingSystem(1, 0, 0);
+            var result = ps.AddCar(1);
+            Assert.IsTrue(result);
+        }
+    }
+    [TestClass]
+    public sealed class CarCantBeAdded
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ParkingSystem ps = new ParkingSystem(1, 0, 0);
+            var result = ps.AddCar(2);
+            Assert.IsFalse(result);
+        }
+    }
+}
