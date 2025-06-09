ddfb1ec47d76406e3624ade64e55d9be63d13477
carter-oleary <carter.m.oleary@outlook.com>
2025-06-09 18:20:51 -0400

    Updated to list instead of individual studs

--- Assignment_10.1/Program.cs
+++ Assignment_10.1/Program.cs
@@ -4,17 +4,21 @@ using System.Xml.Serialization;
 
 Console.WriteLine("----Assignment 10.1----");
 
-Student? student = new Student(1, "Carter", "OLeary");
-string jsonSer = JsonSerializer.Serialize(student);
+var list = new List<Student>();
+
+list.Add(new Student(1, "Carter", "OLeary"));
+list.Add(new Student(2, "John", "Doe"));
+string jsonSer = JsonSerializer.Serialize(list);
 Console.WriteLine($"Json String: {jsonSer}");
-student = JsonSerializer.Deserialize<Student>(jsonSer);
-Console.WriteLine(student.ToString());
+list = JsonSerializer.Deserialize<List<Student>>(jsonSer);
+foreach(Student s  in list) Console.WriteLine(s);
+
 
 string file = @"C:\Users\Carter\Desktop\test\test.txt";
-var xmlSer = new XmlSerializer(typeof(Student));
+var xmlSer = new XmlSerializer(typeof(List<Student>));
 var writer = new StreamWriter(file);
-xmlSer.Serialize(writer, student);
+xmlSer.Serialize(writer, list);
 writer.Close();
 var reader = new StreamReader(file);
-var xmlData = xmlSer.Deserialize(reader) as Student;
-Console.WriteLine(xmlData);
+var xmlData = xmlSer.Deserialize(reader) as List<Student>;
+foreach (Student s in xmlData) Console.WriteLine(s);
