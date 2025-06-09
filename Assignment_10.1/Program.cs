8acb57ffee60736c0999059cd95bb0ea2a349716
carter-oleary <carter.m.oleary@outlook.com>
2025-06-09 14:13:28 -0400

    Assignment 10.1

---
+++ Assignment_10.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+Console.WriteLine("----Assignment 10.1----");
+
+Student? student = new Student(1, "Carter", "OLeary");
+string jsonSer = JsonSerializer.Serialize(student);
+Console.WriteLine($"Json String: {jsonSer}");
+student = JsonSerializer.Deserialize<Student>(jsonSer);
+Console.WriteLine(student.ToString());
+
+string file = @"C:\Users\Carter\Desktop\test\test.txt";
+var xmlSer = new XmlSerializer(typeof(Student));
+var writer = new StreamWriter(file);
+xmlSer.Serialize(writer, student);
+writer.Close();
+var reader = new StreamReader(file);
+var xmlData = xmlSer.Deserialize(reader) as Student;
+Console.WriteLine(xmlData);
---
+++ Assignment_10.1/Student.cs
@@ -0,0 +1,21 @@
+﻿public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public Student(int id, string firstName, string lastName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public Student() { }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}\nName: {LastName}, {FirstName}";
+    }
+}
+
