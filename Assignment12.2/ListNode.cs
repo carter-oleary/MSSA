ba4ed9dc9c22b26dab33b17f1efe738ed7859845
carter-oleary <carter.m.oleary@outlook.com>
2025-06-24 16:50:53 -0400

    Assignment 12.2

---
+++ Assignment12.2/ListNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment12._2
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public List<int> ToList()
+        {
+            List<int> result = new List<int>();
+            ListNode current = this;
+            while (current != null)
+            {
+                result.Add(current.val);
+                current = current.next;
+            }
+            return result;
+        }
+    }
+}
---
+++ Assignment12.2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Assignment12._2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode test = new ListNode(1, new ListNode(2, new ListNode(6, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6)))))));
+            Console.WriteLine("Original List:");
+            PrintList(test);
+            ListNode result = RemoveElements(test, 6);
+            Console.WriteLine("List after removing elements with value 6:");
+            PrintList(result);
+        }
+
+        public static ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null) return null;
+            while (head.val == val)
+            {
+                head = head.next;
+                if (head == null) return null; // If all nodes are removed
+            }
+            ListNode current = head;
+            while (current.next != null)
+            {
+                if (current.next.val == val)
+                {
+                    current.next = current.next.next; // Remove the node
+                }
+                else
+                {
+                    current = current.next; // Move to the next node
+                }
+            }
+
+            return head;
+        }
+
+        public static void PrintList(ListNode head)
+        {
+            ListNode current = head;
+            while (current != null)
+            {
+                Console.Write(current.val + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
