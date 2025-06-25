a5db5867f4b78b10e05b9b12bbbf0dc6d79d338f
carter-oleary <carter.m.oleary@outlook.com>
2025-06-25 17:06:10 -0400

    Assignment 12.3

---
+++ Assignment12.3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment12._3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Assignment 12.3----");
+            string[] s = { "abbaca", "azxxzy" };
+            foreach(string str in s)
+            {
+                Console.WriteLine($"Original string: {str}");
+                Console.WriteLine($"Removed string: {RemoveDuplicates(str)}");
+            }
+        }
+
+        public static string RemoveDuplicates(string s)
+        {
+            if(s.Length <= 1) return s;
+
+            var chars = new Stack<char>();
+            foreach (char c in s)
+            {
+                if(chars.Count == 0)
+                {
+                    chars.Push(c);
+                    continue;
+                }   
+                if(c == chars.Peek())
+                {
+                    chars.Pop();
+                    continue;
+                }
+                chars.Push(c);
+            }
+            return new string(chars.Reverse().ToArray());
+
+        }
+    }
+}
